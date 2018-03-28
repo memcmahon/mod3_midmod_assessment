870bc81639143b40b2e9e5cf1577ee3307dac5d8
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-28 09:55:34 -0600

    Works on spiking api call and station creation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,27 @@
 class SearchController < ApplicationController
   def index
     binding.pry
-    @stations = StationSearch.find_by_zip(params[:q])
+    @stations = StationSearch.new(params[:q]).all
   end
 end
+
+class StationSearch
+  def initialize(zip_code)
+    @location   = zip_code
+    @radius     = "6.0"
+    @fuel_type  = "LPG, ELEC"
+    @limit      = "10"
+  end
+
+  def all
+    response = NRELSearchService.find_by(location, radius, fuel_type, limit)
+    StationSearchResult.new(response).stations
+  end
+
+  private
+    attr_readers :location, :radius, :fuel_type, :limit
+end
+
+class NRELSearchService
+  def self.find_by(location, radius, fuel_type, limit)
+    conn = Faraday.new("")
