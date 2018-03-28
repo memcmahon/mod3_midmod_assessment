1a69f8f269338f8cfdddb20b0e9003c0b75f67c7
memcmahon <mcmahon.meganelizabeth@gmail.com>
2018-03-28 10:52:21 -0600

    Adds webmock to feature test

--- spec/features/user_can_search_for_stations_by_zip_spec.rb
+++ spec/features/user_can_search_for_stations_by_zip_spec.rb
@@ -3,6 +3,11 @@ require 'rails_helper'
 describe "As a user" do
   describe "When I visit root" do
     it "I can search for stations by index" do
+      json_response = File.open("./spec/fixtures/stations.json")
+
+      stub_request(:get, "https://developer.nrel.gov/api/alt-fuel-stations/v1/nearest.json?api_key=#{ENV["NREL_API_KEY"]}&fuel_type=LPG,%20ELEC&limit=10&location=80203&radius=6.0").
+      to_return(status: 200, body: json_response, headers: {})
+
       visit "/"
 
       fill_in "q", with: "80203"
